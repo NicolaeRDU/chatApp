4b5d848dc6356b97a59affa2eb7b3f8ccb866add
Radu <mihairadu8039@gmail.com>
2025-02-16 21:47:50 +0200

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chats from "./Chats";
 
-const socket = io.connect("https://chat-app-server3.vercel.app/");
+const socket = io.connect("/api");
 
 export default function App() {
   const [username, setUsername] = useState("");
