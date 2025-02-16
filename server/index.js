7879f40f25bf59ffb1cf69e8c8d3d8ac843135d9
Radu <mihairadu8039@gmail.com>
2025-02-16 08:30:19 +0200

    updates

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
     methods: ["GET", "POST"],
+    credentials: true,
   },
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello");
 });
 
 io.on("connection", (socket) => {
