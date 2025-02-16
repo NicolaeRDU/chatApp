2cffeb99740f885b8e5eed9949ecb601f1c37f3a
Radu <mihairadu8039@gmail.com>
2025-02-16 20:40:20 +0200

    update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-client-mocha-five.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
