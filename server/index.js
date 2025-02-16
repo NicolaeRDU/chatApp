bd3f200de740bd7016ab35378d5f44446300b5c1
Radu Mihai Nicolae <92977722+NicolaeRDU@users.noreply.github.com>
2025-02-16 07:58:34 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-rust-tau-39.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
